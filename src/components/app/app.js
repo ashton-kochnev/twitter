1207b1eac2906f45bde786c6e5e4175fb3b4b9ce
ashton kochnev <eminem19901111@gmail.com>
2021-03-28 19:00:02 +0300

    start

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel/search-panel';
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel /> 
+        </div>
+    )
+}
+
+export default App;
